2ffaa249fe11a71893c6a22aa09c6ed1418099a5
leninqwerty03@gmail.com <Deltaforge@2025>
2025-06-28 19:29:08 +0530

    Added files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='0.1.0',
+    author='Shyamchandar', 
+    author_email='leninqwerty03@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+) 
